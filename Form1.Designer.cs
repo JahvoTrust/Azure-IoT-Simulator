82368c9a8650e816e6ad6af2afd96a8172a318ab
KimSangwoo <ap2006kr@naver.com>
2023-01-12 14:47:02 +0900

    INIT

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace IoTSensorSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.txtLogView = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtduration = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(30, 194);
+            this.btnstart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(510, 96);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(574, 194);
+            this.btnstop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(487, 96);
+            this.btnstop.TabIndex = 1;
+            this.btnstop.Text = "stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // txtLogView
+            // 
+            this.txtLogView.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtLogView.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtLogView.Location = new System.Drawing.Point(30, 342);
+            this.txtLogView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLogView.Multiline = true;
+            this.txtLogView.Name = "txtLogView";
+            this.txtLogView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogView.Size = new System.Drawing.Size(1030, 386);
+            this.txtLogView.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IoT Device ConnectionString";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(30, 48);
+            this.txtConnectionString.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(1030, 28);
+            this.txtConnectionString.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Send IoT Data To IoT Hub";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 320);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "센서데이터 발송 간격";
+            // 
+            // txtduration
+            // 
+            this.txtduration.Location = new System.Drawing.Point(209, 102);
+            this.txtduration.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtduration.Name = "txtduration";
+            this.txtduration.Size = new System.Drawing.Size(113, 28);
+            this.txtduration.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(331, 106);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "(밀리초)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 748);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtduration);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLogView);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "IoTSimulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.TextBox txtLogView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtduration;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace IoTSensorSimulator
+{
+    public partial class Form1 : Form
+    {
+        //Connection string for device to cloud messaging
+        private static readonly string connectionString_IoTHub = "";
+
+        //Device Client
+        static DeviceClient truckDeviceClient;
+
+        //Random Generator
+        static Random random = new Random();
+
+        //truck sensor details
+        const double truckTemperature_min = 20;
+        const double truckTemperature_max = 40;
+        static double truckTemperature = 20;
+        const double truckHumidity_min = 50;
+        const double truckHumidity_max = 90;
+        static double truckHumidity = 50;
+        const double truckLattitude_min = 80;
+        const double truckLattitude_max = 120;
+        static double truckLattitude = 80;
+        const double truckLongitude_min = 80;
+        const double truckLongitude_max = 120;
+        static double truckLongitude = 80;
+
+        static int duration = 5000;
+
+        static string output_messages = string.Empty;
+
+        static CancellationTokenSource cts;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            btnstart.Enabled = true;
+            btnstop.Enabled = false;
+            txtConnectionString.Text = connectionString_IoTHub;
+            txtLogView.ReadOnly = true;
+            txtduration.Text = duration.ToString();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cts = new CancellationTokenSource();
+                truckDeviceClient = DeviceClient.CreateFromConnectionString(txtConnectionString.Text);
+
+                SendMessagesToIoTHub(cts.Token);
+                btnstart.Enabled = false;
+                btnstop.Enabled = true;
+                txtLogView.Text = txtLogView.Text + "IoT Simulator Started!!" + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                txtLogView.Text = txtLogView.Text + Environment.NewLine + ex.Message;
+            }
+         
+        }
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+            btnstart.Enabled = true;
+            btnstop.Enabled = false;
+            txtLogView.Text = txtLogView.Text + "IoT Simulator Stopped!!" + Environment.NewLine;
+        }
+
+        private async void SendMessagesToIoTHub(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    truckLattitude = GenerateSensorReading(truckLattitude, truckLattitude_min, truckLattitude_max);
+                    truckLongitude = GenerateSensorReading(truckLongitude, truckLongitude_min, truckLongitude_max);
+                    truckTemperature = GenerateSensorReading(truckTemperature, truckTemperature_min, truckTemperature_max);
+                    truckHumidity = GenerateSensorReading(truckHumidity, truckHumidity_min, truckHumidity_max);
+
+                    var json = CreateJSON(truckTemperature, truckHumidity, truckLattitude, truckLongitude);
+                    var message = CreateMessage(json);
+                    await truckDeviceClient.SendEventAsync(message);
+                    txtLogView.Text = txtLogView.Text +$"Sending message at {DateTime.Now} and Message : "+ Environment.NewLine+$"{json}";
+                    txtLogView.Text = txtLogView.Text + Environment.NewLine;
+                    //Console.WriteLine($"Sending message at {DateTime.Now} and Message : {json}"); 
+                    await Task.Delay(Convert.ToInt32(txtduration.Text));
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLogView.Text = txtLogView.Text + Environment.NewLine + ex.Message;
+                btnstart.Enabled = true;
+                btnstop.Enabled = false;
+            }
+          
+        }
+
+        private double GenerateSensorReading(double currentValue, double min, double max)
+        {
+            double percentage = 5; // 5%
+
+            // generate a new value based on the previous supplied value
+            // The new value will be calculated to be within the threshold specified by the "percentage" variable from the original number.
+            // The value will also always be within the the specified "min" and "max" values.
+            double value = currentValue * (1 + ((percentage / 100) * (2 * random.NextDouble() - 1)));
+
+            value = Math.Max(value, min);
+            value = Math.Min(value, max);
+
+            return value;
+        }
+
+        private string CreateJSON(double tempdata,double humiddata, double lattitudedata, double longitude)
+        {
+            var data = new
+            {
+                temp = tempdata,
+                humid = humiddata,
+                lat = lattitudedata,
+                lngt = longitude
+            };
+            return JsonConvert.SerializeObject(data);
+        }
+
+        private Microsoft.Azure.Devices.Client.Message CreateMessage(string jsonObject)
+        {
+            var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(jsonObject));
+
+            // MESSAGE CONTENT TYPE
+            message.ContentType = "application/json";
+            message.ContentEncoding = "UTF-8";
+
+            return message;
+        }
+    }
+}
